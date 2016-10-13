54fbe79798a2604672e1bfe4a0b3b9c022bfac86
Daniel <danielowen_b@hotmail.com>
2016-10-13 16:13:37 +0200

    working with all files, testing plot

--- task.py
+++ task.py
@@ -58,14 +58,15 @@ def allFiles (conn):
     containerData = conn.get_container("tweets")
     fileNr = 0
     for item in containerData[1]:
-        itemContainer.append(item['name'])
-        while True :
-            try:
-                #AFile = conn.get_object( container="tweets", obj=item['name'])
-                AFile = subprocess.check_call(["curl","-s" ,"-O", "http://130.238.29.253:8080/swift/v1/tweets/"+ item['name'] ])
-                break
-            except:
-                raise
+        if item != (containerData[1])[0]:
+            itemContainer.append(item['name'])
+            while True :
+                try:
+                    #AFile = conn.get_object( container="tweets", obj=item['name'])
+                    AFile = subprocess.check_call(["curl","-s" ,"-O", "http://130.238.29.253:8080/swift/v1/tweets/"+ item['name'] ])
+                    break
+                except:
+                    raise
     conn.close()
     for item in itemContainer:
         fileNr = fileNr + 1
