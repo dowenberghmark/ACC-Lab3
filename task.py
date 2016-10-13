cfc26392e8738f231fe482b973aaa3722c666234
Daniel <danielowen_b@hotmail.com>
2016-10-13 20:36:43 +0200

    testing celery

--- task.py
+++ task.py
@@ -55,7 +55,7 @@ flask_app.config.update(
 )
 
 def make_celery(app):
-    celery = Celery(app.import_name, backend=app.config['CELERY_BACKEND'],
+    celery = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'],
                     broker=app.config['CELERY_BROKER_URL'])
     celery.conf.update(app.config)
     TaskBase = celery.Task
