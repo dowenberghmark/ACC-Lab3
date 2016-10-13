dde6f3dbff421d59380854dbb9a09d47962acbc8
Daniel <danielowen_b@hotmail.com>
2016-10-13 19:57:24 +0200

    testing celery

--- task.py
+++ task.py
@@ -21,18 +21,7 @@ from flask import Flask
 from flask import send_from_directory
 from flask import send_file
 
-def make_celery(app):
-    celery = Celery(app.import_name, backend=app.config['CELERY_BACKEND'],
-                    broker=app.config['CELERY_BROKER_URL'])
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
-    class ContextTask(TaskBase):
-        abstract = True
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-    celery.Task = ContextTask
-    return celery
+
 
 
 # _*_ coding:utf-8 _*_
@@ -61,9 +50,23 @@ occurences = {'han': 0, 'hon': 0, 'hen': 0, 'den': 0,'det': 0,'denna': 0,'denne'
 
 flask_app = Flask(__name__)
 flask_app.config.update(
-    CELERY_BROKER_URL='redis://localhost:6379',
-    CELERY_RESULT_BACKEND='redis://localhost:6379'
+    CELERY_BROKER_URL='amqp',
+    CELERY_RESULT_BACKEND='amqp://mast:pass@127.0.0.1/mast_host'
 )
+
+def make_celery(app):
+    celery = Celery(app.import_name, backend=app.config['CELERY_BACKEND'],
+                    broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
 celery = make_celery(flask_app)
 
 flask_app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
