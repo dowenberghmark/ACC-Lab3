1978b230dace6b396ba66b17abd49d9799c108ab
Daniel <danielowen_b@hotmail.com>
2016-10-13 11:28:00 +0200

    Modified tasks

--- task.py
+++ task.py
@@ -37,18 +37,26 @@ noRetweetsText = ""
 occurences = {'han': 0, 'hon': 0, 'hen': 0, 'den': 0,'det': 0,'denna': 0,'denne': 0}
 def countOccurences(f, occurences):
     noRetweetsText = ""
+    aTweet = ""
+    counter = 0
     ###with open(f, 'r') as k:
-    print ("i'm to big for tweet")
-    for aTweet in f:
-        if aTweet != '\n'  :
-                
+    #print ("i'm to big for tweet")
+    for letter in f:
+        if letter != '\n'  :
+            aTweet += letter
+            counter = 0
+        if letter == '\n' and counter == 0:
+            counter = 1
+            #print (aTweet)
             formatedTweet = json.loads(aTweet)
+            aTweet = ""
             json_data.append(formatedTweet)
             if not formatedTweet["retweeted"]:
                 noRetweetsText = (str(formatedTweet["text"]).lower())
                 counts = Counter(noRetweetsText.split())
                 for find in occurences:    
                     occurences[find] = occurences[find] + counts[find]
+                #break
     
 
 
@@ -57,18 +65,21 @@ def countOccurences(f, occurences):
 def allFiles (conn):
     itemContainer = []
     containerData = conn.get_container("tweets")
-
+    fileNr = 0
     for item in containerData[1]:
+        fileNr = fileNr + 1
         itemContainer.append(item['name'])
         #print (itemContainer[13])
         AFile = conn.get_object( container="tweets", obj=item['name'])
         #AFile = conn.get_object( container="tweets", obj=itemContainer[13])
-        print ("i'm to big for text")
-        target = open("./dump.txt", 'w')
+        print ("Working on File number: " + str(fileNr))
+        #target = open("./dump.txt", 'w')
         text = str(AFile[1].decode("utf-8"))
+        
         #target.write(text)
         #target.close()
         countOccurences(text, occurences)
+        #break
     
         
     print (occurences)
