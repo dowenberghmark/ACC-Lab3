db8b278c979f711b330ab18058acb98dd850f4b4
Daniel <danielowen_b@hotmail.com>
2016-10-13 14:03:20 +0200

    dividing the loops

--- task.py
+++ task.py
@@ -89,7 +89,7 @@ def allFiles (conn):
 
         #target.close()
     for item in itemContainer:
-        print (item['name'])
+        print (str(item['name']))
         countOccurences("./" + str(item['name']), occurences)
         #print (occurences)
         #subprocess.call(["rm", item['name'] ])
